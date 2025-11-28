1d5213a9b387d31239d9bd5019529ec7d682ad25
Nelson Aguiar <nelson.aguiar@b-simple.pt>
2025-11-28 12:24:50 +0000

    cors

--- ChatPrototipo.API/Program.cs
+++ ChatPrototipo.API/Program.cs
@@ -5,12 +5,18 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddSignalR();
 builder.Services.AddCors(options =>
 {
-    options.AddDefaultPolicy(b =>
-        b.AllowAnyHeader()
-         .AllowAnyMethod()
-         .WithOrigins("http://localhost:4200")  // Angular
-         .AllowCredentials()
-    );
+
+    options.AddPolicy("AllowNetlify", policy =>
+    {
+        policy.WithOrigins(
+            "http://localhost:4200",
+            "https://6929915c24b6860008f68e03--chatprototipo.netlify.app",  // Your current preview
+            "https://chatprototipo.netlify.app"  // For prod/custom domain later
+        )
+        .AllowAnyHeader()  // Allows custom headers (e.g., Authorization)
+        .AllowAnyMethod()  // GET, POST, OPTIONS for negotiation
+        .AllowCredentials();  // For auth cookies if used
+    });
 });
 
 var app = builder.Build();
